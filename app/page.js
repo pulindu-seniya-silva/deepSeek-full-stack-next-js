746c0e85ebf7174ec06bb98cbfeaed8d607e3989
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-20 13:18:07 +0530

    feat : mobile version adjustments

--- app/page.js
+++ app/page.js
@@ -11,12 +11,13 @@ export default function Home() {
 
   return (
     <div>
-      <div classname="flex h-screen">
+      <div className="flex h-screen">
         {/* --sidebar-- */}
         <div className = "flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
-          <div className = "md:hidden absolute px-4 to0p-6 flex items-center justify-between w-full">
-            <Image classsName="rotate-180" src={assets.menu_icon}/>
-            <Image classsName="rotate-180" src={assets.menu_icon}/>
+          <div className = "md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
+            <Image onClick={()=> (expand ? setExpand(false) : setExpand(true))}
+            className="rotate-180" src={assets.menu_icon} alt=""/>
+            <Image className="opacity-70" src={assets.chat_icon} alt=""/>
           </div>
         </div>
       </div>
