488156abc11581db7b4a159d7b601bcaecf1e201
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-20 12:36:04 +0530

    feat(UI) : add icons images

--- app/page.js
+++ app/page.js
@@ -1,5 +1,25 @@
+'use client'
+import Image from "next/image";
+import { useState} from "react";
+import { assets } from "../assets/assets";
+
 export default function Home() {
+
+  const [expand, setExpand] = useState(false)
+  const [message, setMessages] = useState([])
+  const [isLoading, setIsloading] = useState(false)
+
   return (
-    <div><h1>test</h1></div>
+    <div>
+      <div classname="flex h-screen">
+        {/* --sidebar-- */}
+        <div className = "flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
+          <div className = "md:hidden absolute px-4 to0p-6 flex items-center justify-between w-full">
+            <Image classsName="rotate-180" src={assets.menu_icon}/>
+            <Image classsName="rotate-180" src={assets.menu_icon}/>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
