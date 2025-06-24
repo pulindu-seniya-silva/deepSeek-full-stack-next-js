c3060f7b4794e3f20ad9b32adb1859deadb1cead
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-24 12:56:26 +0530

    feat : add onClick for chatLabel

--- components/ChatLabel.jsx
+++ components/ChatLabel.jsx
@@ -6,7 +6,11 @@ const ChatLabel = ({openMenu, setOpenMenu}) => {
     return (
         <div className='flex items-center justify-between p-2 text-white/80 hover:bg-white/10 rounded-lg text-sm group cursor-pointer'>
             <p className='group-hover:max-w-5/6 truncate'>Chat Name Here</p>
-            <div className='group relative flex items-center justify-center h-6 w-6 aspect-asquare hover:bg-black/80 rounded-lg'>
+            <div className='group relative flex items-center justify-center h-6 w-6 aspect-asquare hover:bg-black/80 rounded-lg' 
+             onClick={(e) => {
+                    e.stopPropagation(); // Prevent event bubbling to parent
+                    setOpenMenu(prev => ({id: 1, open: !prev.open}));
+                }}>
                 <Image src={assets.three_dots} alt='' className={`w-4 ${openMenu.open ? '' : 'hidden'} group-hover:block`}/>
                 <div className={`absolute ${openMenu.open ? 'block' : 'hidden'} -right-36 top-6 bg-gray-700 rounded-xl w-max p-2`}>
                     <div className='flex items-center gap-3 hover'>
@@ -14,7 +18,7 @@ const ChatLabel = ({openMenu, setOpenMenu}) => {
                         <p>Rename</p>
                     </div>
                     <div className='flex items-center gap-3 hover'>
-                        <Image src={assets.pencil_icon} alt='' className='w-4'/>
+                        <Image src={assets.delete_icon} alt='' className='w-4'/>
                         <p>Delete</p>
                     </div>
                 </div>
