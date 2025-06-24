89654137d446be99817f3a370fca8aac0af5f767
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-24 10:49:57 +0530

    feat : login context

---
+++ context/AppContext.jsx
@@ -0,0 +1,17 @@
+"use client"
+import { createContext} from "react";
+
+export const AppContext = createContext();
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
+
+export const AppContextProvider = ({children}) => {
+    const {user} = useUser()
+
+    const value = {
+        user
+    }
+    return <AppContext.Provider value={value} >{children}</AppContext.Provider>
+}
