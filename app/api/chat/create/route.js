a2de00be503b596fbf7004be91edf33b89c65b3e
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-26 10:38:15 +0530

    feat(BACKEND) : chat create route

---
+++ app/api/chat/create/route.js
@@ -0,0 +1,30 @@
+import connectDB from "../../../../configs/db";
+import Chat from "../../../../models/chat"
+import { getAuth } from "@/clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req)
+
+        if(!userId) {
+            return NextResponse.json({success: false, message: "user not authenticated", })
+        }
+        //prepare the chat data to be saved in the database
+
+        const chatData = {
+            userId,
+            messages: [],
+            name: "New Chat",
+        };
+
+        //Connect to the database and create a new chat
+        await connectDB();
+        await Chat.create(chatData);
+
+        return NextResponse.json({ suceess: true, message: "Chat created" })
+
+    } catch (error) {
+        return NextResponse.json({ suceess: false, error: error.message })
+    }
+}
