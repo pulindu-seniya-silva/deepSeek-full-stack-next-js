13098e0a7e561e9753d6622a5ee9f60fa6fda868
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-26 13:05:23 +0530

    feat : axios call to connect backend and frontend

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,6 +1,8 @@
 "use client"
 import { createContext, useContext} from "react";
 import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -10,6 +12,31 @@ export const useAppContext = () => {
 
 export const AppContextProvider = ({children}) => {
     const {user} = useUser()
+    const {getToken} = useAuth()
+
+    const [chats, setChats] = useState([]);
+    const [selectedChat, selectedSetChat] = useState(null);
+
+    const createNewChat = async () => {
+        try {
+            if(!user) return null;
+            const token = await getToken();
+
+            await axios.post('/api/chat/create', {}, {headers:{
+                Authorization: `Bearer ${token}`
+            }})
+        } catch (error) {
+            toast.error(errorToJSON.message)
+        }
+    }
+
+    const fetchUsersChat = async () => {
+        try {
+            const token = await getToken();
+        } catch (error) {
+            
+        }
+    }
 
     const value = {
         user
