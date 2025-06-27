2861dd4cec3606921fb22864d24911dac9fd0d84
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-06-27 08:42:38 +0530

    feat : loarding untill response print

--- app/page.jsx
+++ app/page.jsx
@@ -40,7 +40,7 @@ export default function Home() {
             <Image className="opacity-70" src={assets.chat_icon} alt=""/>
           </div>
 
-          {message.length === 0 ? (
+          {messages.length === 0 ? (
             <>
               <div className="flex items-center gap-3">
                 <Image src={assets.logo_icon} alt="" className="h-16"/>
@@ -50,11 +50,28 @@ export default function Home() {
             </>
           ):
           (
-          <div ref={containerRef}>
+          <div ref={containerRef}
+          classname="relative flex flex-col items-center justify-start w-full mt-20 max-h-screen overflow-y-auto">
             <p className="fixed top-8 border-transparent hover:border-gray-500/50 py-1 px-2 rounded-lg font-semibold mb-6">{selectedChat.name}</p>
             {messages.map((msg, index)=>(
                 <Message key={index} role={msg.role} content={msg.content}/>
             ))}
+
+            {
+              isLoading && (
+                <div className="flex gap-4 max-w-3xl w-full py-3">
+                  <Image className="h-9 w-9 p-1 border border-white/15 rounded-full" src={assets.logo_icon} alt="Logo"/>
+                  <div className="loader flex justify-center items-center gap-1">
+                    <div className="w-1 h-1 rounded-full bg-white animate-bounce">
+                    </div>
+                    <div className="w-1 h-1 rounded-full bg-white animate-bounce">
+                    </div>
+                    <div className="w-1 h-1 rounded-full bg-white animate-bounce">
+                    </div>
+                  </div>
+                </div>
+              )
+            }
             
           </div>
         )
